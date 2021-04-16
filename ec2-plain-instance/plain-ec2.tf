d81fb7b3786de6f22fdeeff5f4f1f92ddce1e12c
limbuu <iammanshi116@gmail.com>
2021-04-16 21:03:40 +0545

    added terraform templates for ec2 instance with default configuration

---
+++ ec2-plain-instance/plain-ec2.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-013f17f36f8b1fefb"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "AWS-Plain-EC2-Instance-By-Manshi"
+  }
+}
+
+output "ip" {
+ description = "These are the public Ip of EC2 instance"
+ value = aws_instance.testInstance.public_ip
+}
+
+
